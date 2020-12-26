89a3c283a52f599ba759348caf0297b77e3f46d4
Alexis Barba <alexbarba.2278@gmail.com>
2020-12-26 10:17:59 -0700

    [add] Card Component

---
+++ src/Card.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FaHeart } from 'react-icons/fa';
+import { FaRegHeart } from 'react-icons/fa';
+
+const Card = ({ props }) => {
+	const { title, description, image, handleFavorite, favorite } = props;
+	return (
+		<div className="max-w-xs justify-self-center rounded overflow-hidden shadow-lg my-2 dark:bg-gray-600 bg-red-100">
+			<img className="w-full" src={image} alt="Sunset in the mountains" />
+			<div className="px-6 py-4">
+				<div className="font-bold text-xl mb-2">{title}</div>
+				<p className="text-grey-darker text-base">{description}</p>
+			</div>
+
+			<span
+				className="inline-block bg-grey-lighter rounded-full text-xl font-semibold text-grey-darker"
+				onClick={() => handleFavorite()}
+			>
+				{favorite ? <FaHeart /> : <FaRegHeart />}
+			</span>
+		</div>
+	);
+};
+
+export default Card;
